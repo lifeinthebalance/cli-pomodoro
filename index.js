d4ff89a516c61aebad11e212e66f77bb5a48d7e6
blackrain313 <sickl3im@gmail.com>
2023-12-05 19:15:09 +0300

    adw

--- index.js
+++ index.js
@@ -3,17 +3,15 @@
 import inquirer from "inquirer";
 import ansiEscapes from "ansi-escapes";
 
-let time = {
-  work: 25,
-  break: 5,
-};
+// let time = {
+//   work: 25,
+//   break: 5,
+// };
 
 let cycleHandler = 0; //amount of cycles completed
 let stateHandler = 1; //current sstate
 
 function workCountdown(flow, pause) {
-  flow = Number(flow) - 1;
-  pause = Number(pause);
   let sec = 60;
   console.log(stateHandler);
 
@@ -23,34 +21,28 @@ function workCountdown(flow, pause) {
   // }
 
 
-  const stateTimer = setInterval(() => {
+  setInterval(() => {
 
-    if ((flow === 0) && (sec === 0)) { //changes between states
+    if ((flow == 0) && (sec == 55)) {
       stateHandler = 0;
       console.log(stateHandler);
-    } else if ((pause === 0) && (sec === 0)) {
-      stateHandler = 1;
-      console.log(stateHandler);
     }
+
+
 
     process.stdout.write(
       stateHandler
         ? ansiEscapes.eraseLines(1) +
-        `${flow}:${sec <= 9 ? "0" + (sec -= 1) : (sec -= 1)}`
+        `${flow - 1}:${sec <= 10 ? "0" + (sec -= 1) : (sec -= 1)}`
         : ansiEscapes.eraseLines(1) +
         `${pause}:${sec <= 10 ? "0" + (sec -= 1) : (sec -= 1)}`
     );
+
+
+
   }, 1000);
 
 
-
-  if ((sec === 55) && stateHandler) {
-    flow -= 1;
-    sec = 60;
-  } else {
-    pause -= 1;
-    sec = 60;
-  }
 }
 
 async function start() {
@@ -63,12 +55,15 @@ async function start() {
     },
   });
   const result = schedule.method.split("-");
-  time.work = Number(result[0]);
-  time.break = Number(result[1]);
 
   console.clear();
+  workCountdown(Number(result[0]), Number(result[1]));
 
-  workCountdown(time.work, time.break);
+
+
 }
+
+
+
 
 await start();
