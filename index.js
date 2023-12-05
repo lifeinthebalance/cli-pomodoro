2dc429042ead3486075c200e5872731af98388e5
blackrain313 <sickl3im@gmail.com>
2023-12-05 19:18:35 +0300

    Revert "adding handlers and states"
    This reverts commit 38452efc041218b773b702d9300b4ac74abe3540.

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ let cycleHandler = 0; //amount of cycles completed
 let stateHandler = 1; //current sstate
 
 function workCountdown(flow, pause) {
+  flow = Number(flow) - 1;
+  pause = Number(pause);
   let sec = 60;
   console.log(stateHandler);
 
@@ -21,28 +23,34 @@ function workCountdown(flow, pause) {
   // }
 
 
-  setInterval(() => {
+  const stateTimer = setInterval(() => {
 
-    if ((flow == 0) && (sec == 55)) {
+    if ((flow === 0) && (sec === 0)) { //changes between states
       stateHandler = 0;
       console.log(stateHandler);
+    } else if ((pause === 0) && (sec === 0)) {
+      stateHandler = 1;
+      console.log(stateHandler);
     }
-
-
 
     process.stdout.write(
       stateHandler
         ? ansiEscapes.eraseLines(1) +
-        `${flow - 1}:${sec <= 10 ? "0" + (sec -= 1) : (sec -= 1)}`
+        `${flow}:${sec <= 9 ? "0" + (sec -= 1) : (sec -= 1)}`
         : ansiEscapes.eraseLines(1) +
         `${pause}:${sec <= 10 ? "0" + (sec -= 1) : (sec -= 1)}`
     );
-
-
-
   }, 1000);
 
 
+
+  if ((sec === 55) && stateHandler) {
+    flow -= 1;
+    sec = 60;
+  } else {
+    pause -= 1;
+    sec = 60;
+  }
 }
 
 async function start() {
@@ -61,6 +69,7 @@ async function start() {
 
 
 
+  workCountdown(time.work, time.break);
 }
 
 
