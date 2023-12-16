968d0618392206588b78399250dc645679eba09f
blackrain313 <sickl3im@gmail.com>
2023-12-16 17:17:55 +0300

    rewrote the main func

--- index.js
+++ index.js
@@ -3,30 +3,60 @@
 import inquirer from "inquirer";
 import ansiEscapes from "ansi-escapes";
 
-let time = {
-  work: 25,
-  break: 5,
-};
+let workDuration = 0;
+let breakDuration = 0;
+
+async function startPomodoro() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'work',
+      message: 'work tiem?',
+      default: '25',
+    },
+    {
+      type: 'input',
+      name: 'break',
+      message: 'break time?',
+      default: '5',
+    },
+  ]).then((answers) => {
+    console.clear();
+    inquirer.prompt({
+      type: 'input',
+      name: 'start',
+      message: '>',
+      default: 'type "start" when ready'
+    }).then(({ start }) => {
+      if (start == 'start') {
+        console.clear();
+        workDuration = Number(answers.work);
+        breakDuration = Number(answers.break);
+        workCountdown(workDuration, breakDuration);
+      }
+    })
+
+  })
 
 
-let cycleHandler = 0; //amount of cycles completed
-let stateHandler = 1; //current sstate
+}
+
+await startPomodoro();
 
 function workCountdown(num1, num2) {
   let work = num1 - 1;
   let rest = num2 - 1;
   let sec = 60;
+  let stateHandler = 1; //current sstate
+  let cycleHandler = 0; //amount of cycles completed
 
-  setInterval(() => {
+  const timer = setInterval(() => {
 
 
     if (stateHandler) {
       process.stdout.write(ansiEscapes.eraseLines(1) + `${work}:${sec <= 10 ? "0" + (sec -= 1) : (sec -= 1)}`);
 
-      if (work === 0 && sec === 0) {
-        sec = 60;
-        stateHandler = 0;
-      } else if (sec === 0) {
+      if (sec === 0) {
         work -= 1;
         sec = 60;
       }
@@ -35,36 +65,44 @@ function workCountdown(num1, num2) {
     if (!stateHandler) {
       process.stdout.write(ansiEscapes.eraseLines(1) + `${rest}:${sec <= 10 ? "0" + (sec -= 1) : (sec -= 1)}`);
 
-      if (rest === 0 && sec === 0) {
-        sec = 60;
-        stateHandler = 1;
-        cycleHandler += 1;
-      } else if (sec === 0) {
+      if (sec === 0) {
         rest -= 1;
         sec = 60;
       }
     }
 
+    if ((work === 0 && sec === 55)) {
+      clearInterval(timer);
+      console.clear();
+      inquirer.prompt({
+        type: 'input',
+        name: 'period',
+        message: '>'
+      }).then(({ period }) => {
+        if (period === 'break') {
+          workCountdown(workDuration, breakDuration)
+        }
+      })
+    }
+
   }, 1000);
-
 }
 
-async function start() {
-  const schedule = await inquirer.prompt({
-    name: "method",
-    type: "input",
-    message: "work time?",
-    default() {
-      return "25-5";
-    },
-  });
-  const result = schedule.method.split("-");
-  console.clear();
+// async function start() {
+//   const schedule = await inquirer.prompt({
+//     name: "method",
+//     type: "input",
+//     message: "work time?",
+//     default() {
+//       return "25-5";
+//     },
+//   });
+//   const result = schedule.method.split("-");
+//   console.clear();
 
-  workCountdown(Number(result[0]), Number(result[1]));
-}
+//   workCountdown(Number(result[0]), Number(result[1]));
+// }
+
+// await start();
 
 
-
-
-await start();
