37514d8cb16d99dd45e97ce87df42874b3326a49
blackrain313 <sickl3im@gmail.com>
2024-01-11 22:02:58 +0300

    commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env mode
 
 import inquirer from "inquirer";
-import ansiEscapes from "ansi-escapes";
 import notifier from 'node-notifier';
 import ora from 'ora';
+// import ansiEscapes from "ansi-escapes";
 
 let workDuration = 0;
 let breakDuration = 0;
@@ -15,12 +15,26 @@ async function startPomodoro() {
       name: 'work',
       message: 'Enter work duration',
       default: '25',
+      validate(answer) {
+        if (answer.match('[0-9]+')) {
+          return true;
+        }
+
+        return 'Must be a number'
+      },
     },
     {
       type: 'input',
       name: 'break',
       message: 'Enter break duration',
       default: '5',
+      validate(answer) {
+        if (answer.match('[0-9]+')) {
+          return true;
+        }
+
+        return 'Must be a number'
+      },
     },
   ]).then((answers) => {
     console.clear();
@@ -28,14 +42,19 @@ async function startPomodoro() {
       type: 'input',
       name: 'starter',
       message: '>',
-      default: 'type "start" when ready'
-    }).then(({ starter }) => {
-      if (starter === 'start') {
-        console.clear();
-        workDuration = Number(answers.work);
-        breakDuration = Number(answers.break);
-        pomodoroTimer(workDuration, breakDuration);
+      default: 'type "start" when ready',
+      validate(answer) {
+        if (answer !== 'start') {
+          return 'enter "start" to start the clock'
+        }
+
+        return true;
       }
+    }).then(({ starter }) => {
+      console.clear();
+      workDuration = Number(answers.work);
+      breakDuration = Number(answers.break);
+      pomodoroTimer(workDuration, breakDuration);
     })
   })
 }
@@ -43,13 +62,16 @@ async function startPomodoro() {
 
 let cycles = 0; //amount of cycles completed
 
-function pomodoroTimer(num1, num2) {
-  let rest = ((cycles > 0) && (cycles % 4 === 0)) ? num2 * 4 - 1 : num2 - 1;
+function pomodoroTimer(workD, breakD) {
+  // let rest = ((cycles > 0) && (cycles % 4 === 0)) ? work * 4 - 1 : work - 1;
   // let sec = 60;
   // let state = 1; //current sstate
+  if ((cycles > 0) && !(cycles % 4)) {
+    breakD *= 4;
+  }
 
-  let workTimestamp = Date.now() + (num1) * 60000;
-  let breakTinestamp = Date.now() + (num2) * 60000;
+  let workTimestamp = Date.now() + (workD) * 60000;
+  let breakTinestamp = Date.now() + (workD + breakD) * 60000;
   let workEndHours = new Date(workTimestamp).getHours();
   let workEndMinutes = new Date(workTimestamp).getMinutes();
   let breakEndHours = new Date(breakTinestamp).getHours();
@@ -62,7 +84,7 @@ function pomodoroTimer(num1, num2) {
 
   setTimeout(() => {
     workOra.stop();
-    // console.clear();
+    cycles += 1;
     notifier.notify({
       title: 'Pomodoro',
       message: 'Take a break!',
@@ -78,17 +100,17 @@ function pomodoroTimer(num1, num2) {
       });
       inquirer.prompt({
         type: 'input',
-        name: 'start',
+        name: 'starter',
         message: '>',
         default: 'type "start" when ready',
-      }).then(({ start }) => {
-        if (start == 'start') {
+      }).then(({ starter }) => {
+        if (starter === 'start') {
           console.clear();
           pomodoroTimer(workDuration, breakDuration);
         }
       });
-    }, (rest + 1) * 60000);
-  }, (work + 1) * 60000);
+    }, breakD * 60000);
+  }, workD * 60000);
 
 
   // process.stdout.moveCursor(0, 1);
