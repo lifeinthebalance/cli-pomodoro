ece47a72876bebf893f4a5975e63d1387c6c16a1
blackrain313 <sickl3im@gmail.com>
2024-01-13 11:00:24 +0300

    somewhat ready

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import inquirer from "inquirer";
 import notifier from 'node-notifier';
 import cliProgress from 'cli-progress';
 import chalk from "chalk";
+import gradient from "gradient-string";
 // import ansiEscapes from "ansi-escapes";
 
 let workDuration = 0;
@@ -53,7 +54,6 @@ function startPomodoro() {
         return true;
       }
     }).then(() => {
-      console.clear();
       pomodoroTimer(workDuration, breakDuration);
     })
   })
@@ -63,53 +63,63 @@ function startPomodoro() {
 let cycles = 0; //amount of cycles completed
 
 function pomodoroTimer(workD, breakD) {
-  // let rest = ((cycles > 0) && (cycles % 4 === 0)) ? work * 4 - 1 : work - 1;
-  // let sec = 60;
-  // let state = 1; //current sstate
+
   if ((cycles > 0) && !(cycles % 4)) {
     breakD *= 4;
   }
 
-  let workTimestamp = Date.now() + (workD) * 60000;
-  let breakTinestamp = Date.now() + (workD + breakD) * 60000;
-  let workEndHours = new Date(workTimestamp).getHours();
-  let workEndMinutes = new Date(workTimestamp).getMinutes();
-  let breakEndHours = new Date(breakTinestamp).getHours();
-  let breakEndMinutes = new Date(breakTinestamp).getMinutes();
-  // let endHours = new Date(timestamp).getHours();
-  // let endMinutes = new Date(timestamp).getMinutes();
+  function showOutput(num, state) {
+    let workTimestamp = Date.now() + (workD) * 60000;
+    let breakTinestamp = Date.now() + (workD + breakD) * 60000;
+    let workEndHours = new Date(workTimestamp).getHours();
+    let workEndMinutes = new Date(workTimestamp).getMinutes();
+    let breakEndHours = new Date(breakTinestamp).getHours();
+    let breakEndMinutes = new Date(breakTinestamp).getMinutes();
+
+    console.clear();
 
 
-  process.stdout.write(`flow state until ${workEndHours}:${(workEndMinutes < 10) ? '0' + workEndMinutes : workEndMinutes} \n \n`);
-  // let workOra = ora(`flow state until ${workEndHours}:${(workEndMinutes < 10) ? '0' + workEndMinutes : workEndMinutes} \n`).start();
-  function progressBar(num) {
+    if (state === 'work') {
+      process.stdout.write(gradient('EA5A6F', 'DE791E', 'FCCF3A')(`FOCUSING UNTIL ${workEndHours}:${(workEndMinutes < 10) ? '0' + workEndMinutes : workEndMinutes} 🐒 \n \n`));
+    } else {
+      process.stdout.write(gradient('274B74', '8233C5', 'E963FD')(`TAKING A BREAK UNTIL ${breakEndHours}:${(breakEndMinutes < 10) ? '0' + breakEndMinutes : breakEndMinutes} 🐒 \n \n`));
+    }
+
     const bar = new cliProgress.SingleBar({
-      format: chalk.bgRed('{bar}') + ' {percentage}%',
+      format: chalk.bgMagenta('{bar}') + '  {percentage}%',
     }, cliProgress.Presets.shades_grey);
     bar.start(num, 0);
     const int = setInterval(() => {
       bar.increment(1000);
+      // if (bar.getProgress() === 1) {
+      //   bar.removeListener();
+      // }
     }, 1000);
+
   };
-  progressBar(workD * 60000);
+  showOutput(workD * 60000, 'work');
 
 
   setTimeout(() => {
-    // workOra.stop();
     cycles += 1;
+    showOutput(breakD * 60000, 'break');
+
     notifier.notify({
       title: 'Pomodoro',
       message: 'Take a break!',
       icon: './icon_70.png',
     });
-    // let breakOra = ora(`taking a break until ${breakEndHours}:${(breakEndMinutes < 10) ? '0' + breakEndMinutes : breakEndMinutes}`).start();
+
     setTimeout(() => {
-      // breakOra.stop();
+
       notifier.notify({
         title: 'Pomodoro',
         message: 'Get ready for work!',
         icon: './icon_70.png',
       });
+
+      console.clear();
+
       inquirer.prompt({
         type: 'input',
         name: 'starter',
@@ -121,75 +131,12 @@ function pomodoroTimer(workD, breakD) {
 
           return true;
         }
-      }).then(({ starter }) => {
-        if (starter === 'start') {
-          console.clear();
-          pomodoroTimer(workDuration, breakDuration);
-        }
+      }).then(() => {
+        pomodoroTimer(workDuration, breakDuration);
       });
-    }, breakD * 60000);
+    }, breakD * 60000 + 1000);
   }, workD * 60000);
 
-
-  // process.stdout.moveCursor(0, 1);
-
-  // const timer = setInterval(() => {
-
-  //   ansiEscapes.cursorShow;
-  //   if (state) {
-  //     process.stdout.write(ansiEscapes.eraseLines(1) + ` ${work}:${sec <= 10 ? "0" + (sec -= 1) : (sec -= 1)}`);
-
-  //     if ((sec === 0) && (work > 0)) {
-  //       work -= 1;
-  //       sec = 60;
-  //     }
-
-
-  //     if ((work === 0) && (sec === 55)) {
-
-  //       cycles += 1;
-  //       state = 0;
-  //       sec = 60;
-  // notifier.notify({
-  //   title: 'Pomodoro',
-  //   message: 'Take a break!',
-  //   icon: './icon.webp',
-  // });
-  //     }
-
-
-  //   } else {
-  //     process.stdout.write(ansiEscapes.eraseLines(1) + `taking a break until ${endHours}:${endMinutes}`);
-
-  //     if ((sec === 0) && (rest > 0)) {
-  //       rest -= 1;
-  //       sec = 60;
-  //     }
-  //   }
-
-
-  // if ((sec === 55) && !state) {
-  //   clearInterval(timer);
-  //   console.clear();
-  //   notifier.notify({
-  //     title: 'Pomodoro',
-  //     message: 'Get ready for work!',
-  //     icon: './icon.webp',
-  //   });
-  //   inquirer.prompt({
-  //     type: 'input',
-  //     name: 'start',
-  //     message: '>',
-  //     default: 'type "start" when ready',
-  //   }).then(({ start }) => {
-  //     if (start == 'start') {
-  //       console.clear();
-  //       pomodoroTimer(workDuration, breakDuration);
-  //     }
-  //   })
-  // }
-
-  // }, 1000);
 }
 
 
